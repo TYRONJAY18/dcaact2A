455a4078e8e1d02761bce0e658a8b06fc2f3ebf6
CL2~PC37 <CL2~PC37@DESKTOP-PIE3BAO>
2025-07-11 12:18:00 +0800

    sample program

---
+++ src/main/java/com/mycompany/tyron/Tyron.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tyron;
+
+/**
+ *
+ * @author CL2~PC37
+ */
+public class Tyron {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Tyron Jay Gonzales");
+    }
+}
