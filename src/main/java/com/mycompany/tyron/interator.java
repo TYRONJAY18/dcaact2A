9328924d0c89f119328fdb881b7c105f701f68cf
Tyron Jay <Tyron Jay@Jay>
2025-09-11 20:54:38 +0800

    YUNOHH

---
+++ src/main/java/com/mycompany/tyron/interator.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tyron;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Tyron Jay
+ */
+public class interator {
+    public static void main(String[] args) {
+	 // Create a HashMap object called capitalCities
+    HashMap<String, String> capitalCities = new HashMap <String, String>();
+
+    // Add keys and values (Country, City)
+    capitalCities.put("England", "London");
+    capitalCities.put("Germany", "Berlin");
+    capitalCities.put("Norway", "Oslo");
+    capitalCities.put("USA", "Washington DC");
+    System.out.println(capitalCities);
+	}
+
+}
